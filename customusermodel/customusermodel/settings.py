d4a022cba01859b917655f718e63b3d07d2eea85
KellyDanielO <kizdver@gmail.com>
2023-08-25 19:15:25 +0100

    .

--- customusermodel/customusermodel/settings.py
+++ customusermodel/customusermodel/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'base.app.BaseConfig',
+
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+AUTH_USER_MODEL = 'base.User'
 
 ROOT_URLCONF = 'customusermodel.urls'
 
